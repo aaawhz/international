21a085cd78ca1087841174eae9d0abaf7e981364
hai <whzzyz@21cn.com>
2017-11-01 11:24:25 +0800

    aa

--- tf.js
+++ tf.js
@@ -1,6 +1,6 @@
 function enunicode(code){
-code=code.replace(/[\u00FF-\uFFFF]/g,function($0){
-		return '\\u'+$0.charCodeAt().toString(16);
-});
-return code;
+	code=code.replace(/[\u00FF-\uFFFF]/g,function($0){
+			return '\\u'+$0.charCodeAt().toString(16);
+	});
+	return code;
 };
