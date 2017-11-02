bec0c44950c00a667b56333fbe89042677f7a547
hai <whzzyz@21cn.com>
2017-11-02 10:34:10 +0800

    aa

--- read/regs.js
+++ read/regs.js
@@ -9,11 +9,33 @@ var ncname = '[a-zA-Z_][\\w\\-\\.]*';
 var qnameCapture = '((?:' + ncname + '\\:)?' + ncname + ')';
 
 
-var startTagOpen = new RegExp('^<' + qnameCapture);
+var startTagOpen = new RegExp('<' + qnameCapture);
 
-var startTag = new RegExp('^<' + qnameCapture + '[^>]*>');
+var startTag = new RegExp('<' + qnameCapture + '[^>]*>');
 
 //'</div>'.match(endTag)
 // ["</div>", "div", index: 0, input: "</div>"]
-var endTag = new RegExp('^<\\/' + qnameCapture + '[^>]*>');
+var endTag = new RegExp('<\\/' + qnameCapture + '[^>]*>');
+
+var getTag = new RegExp('<(\\/)?' + qnameCapture + '[^>]*>', 'g');
  
+ //'拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。<br><br>                         <input id="editorSpellChkTip" type="checkbox"/>&nbsp;不再提示'
+ //拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。
+ //<br><br>                          <input id="editorSpellChkTip" type="checkbox"/>
+ //&nbsp;不再提示
+
+
+ var str = '<br><br> abc  <input> ddd ';
+ 
+ //字符串是否只有容许的标签， 如果 'abc<br/>好滴' 就把这个整体提取出来做国际化， 简单易行
+ var onlyAllowTag = true;
+ //如果有开始标签
+ if( getTag.test(str) ){
+ 	str.replace(getTag, function ($s) {
+ 		if(!allowTag.test($s)){
+ 			onlyAllowTag = false;
+ 		}
+ 	})
+ }
+
+ console.log(onlyAllowTag);
