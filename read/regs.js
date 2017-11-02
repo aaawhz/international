c2a142523745a712669fd4139950adfdc62348be
hai <whzzyz@21cn.com>
2017-11-02 10:49:40 +0800

    aa

--- read/regs.js
+++ read/regs.js
@@ -2,6 +2,7 @@
 //匹配 <br> </br> </  br>  或者  <br  /> <br/>
 var allowTag = /<br\s{0,20}(\/?)>|<(\/?)\s{0,20}br>/;
 
+
 //ncname匹配的是以a-zA-Z_开头，然后是0或多个a-zA-Z_、-或.。
 var ncname = '[a-zA-Z_][\\w\\-\\.]*';
 
@@ -18,18 +19,19 @@ var startTag = new RegExp('<' + qnameCapture + '[^>]*>');
 var endTag = new RegExp('<\\/' + qnameCapture + '[^>]*>');
 
 var getTag = new RegExp('<(\\/)?' + qnameCapture + '[^>]*>', 'g');
- 
- //'拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。<br><br>                         <input id="editorSpellChkTip" type="checkbox"/>&nbsp;不再提示'
- //拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。
- //<br><br>                          <input id="editorSpellChkTip" type="checkbox"/>
- //&nbsp;不再提示
+
+var Util = {
 
 
- var str = '<br><br> abc  <input> ddd ';
- 
+ haszh: function (str){
+ 	//中文
+	var zhRe = /[\u4E00-\u9FA5]/;
+
+ 	return zhRe.test(str);
+ },
 
 //检查字符串是否符合国际化要求， 返回true就全部提取出来， 返回false则进入标签解析， 分段提取
- function checkTag(str){
+ checkTag: function (str){
  	//字符串是否只有容许的标签， 如果 'abc<br/>好滴' 就把这个整体提取出来做国际化， 简单易行
 	 var onlyAllowTag = true;
 	 //如果有开始标签
@@ -46,6 +48,40 @@ var getTag = new RegExp('<(\\/)?' + qnameCapture + '[^>]*>', 'g');
 	 	return onlyAllowTag;
 	 }
  }
+
+};
+
  
+ //'拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。<br><br>                         <input id="editorSpellChkTip" type="checkbox"/>&nbsp;不再提示'
+ //拼写检查暂不支持中文。不过，我们仍可以检查您邮件中的所有英文拼写。
+ //<br><br>                          <input id="editorSpellChkTip" type="checkbox"/>
+ //&nbsp;不再提示
+
+
+ var str = '<br><br> 葫芦娃  <input> 变形金刚 ';
+ 
+
+ if(Util.haszh(str)){
+ 	if(Util.checkTag(str)){
+ 		//进入提取中文
+ 		pickText();
+ 	}else{
+ 		//str全部提取， 直接转换
+ 	}
+ }else{
+ 	//跳过
+ }
+  
+
+
+function pickText(str){
+
+}
+
+
+
+function transform(str){
+
+};
 
  console.log(onlyAllowTag);
