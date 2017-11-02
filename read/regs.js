b7652c37be64e78a065e9dbb8713fd0fbb1364d9
hai <whzzyz@21cn.com>
2017-11-02 10:00:41 +0800

    aa

--- read/regs.js
+++ read/regs.js
@@ -5,14 +5,15 @@ var allowTag = /<br\s{0,20}(\/?)>|<(\/?)\s{0,20}br>/;
 //ncname匹配的是以a-zA-Z_开头，然后是0或多个a-zA-Z_、-或.。
 var ncname = '[a-zA-Z_][\\w\\-\\.]*';
 
-//(?:' + ncname + '\\:)? 这个主要是为了兼容这样的标签 <svg:path.test />， 是ncname的加强
+//(?:' + ncname + '\\:)? 这个主要是为了兼容这样的标签 <svg:path.test /> => <svg:path.test， 是ncname的加强
 var qnameCapture = '((?:' + ncname + '\\:)?' + ncname + ')';
 
 
 var startTagOpen = new RegExp('^<' + qnameCapture);
 
+var startTag = new RegExp('^<' + qnameCapture + '[^>]*>');
+
 //'</div>'.match(endTag)
 // ["</div>", "div", index: 0, input: "</div>"]
 var endTag = new RegExp('^<\\/' + qnameCapture + '[^>]*>');
-
-
+ 
